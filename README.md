b42edaba4d576d3073682c1717c91525e80c76d4
rui.zhang <rui.zhang@tendcloud.com>
2020-02-23 20:18:26 +0800

    调整配置文件
    Change-Id: I28a88a34b1349e40ae6033fe0679e653a22172f6

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # Calf
 
+## Summary
+
 一个基于Java SpringBoot实现的CMS服务。有许多尚未完善之处，只是初生的小牛。
+
+## Install
+
+启动文件是`calf.service`。
+
+启动文件需要被置于如下路径：
+
+```shell script
+/usr/lib/systemd/system/
+```
+
+相关指令：
+
+```shell script
+# 修改 service 文件之后需要刷新 Systemd
+sudo systemctl daemon-reload
+
+# 使 Calf 开机自启
+sudo systemctl enable calf
+
+# 启动 Calf
+sudo service calf start
+
+# 重启 Calf
+sudo service calf restart
+
+# 停止 Calf
+sudo service calf stop
+
+# 查看 Calf 的运行状态
+sudo service calf status
+```
--- src/test/java/org/chobit/calf/MyApp.java
+++ src/test/java/org/chobit/calf/MyApp.java
@@ -1,24 +1,11 @@
 package org.chobit.calf;
 
 
-import org.chobit.calf.model.RemoteRequest;
-
-import java.io.FileNotFoundException;
-
-import static sun.plugin2.util.PojoUtil.toJson;
-
 public class MyApp {
 
-    private static final String PATTERN_SHORT = "^第?[\\s]{0,9}[\\d〇零一二三四五六七八九十百千万]{1,6}[\\s]{0,9}[章回节卷篇讲卷集]([\\s]{1,9}.{0,32})?$";
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        RemoteRequest request = new RemoteRequest();
-        request.setWorkName("念奴娇");
-        request.setChapterName("hehe");
-        request.setContent("-------------------------zzzzzz");
-        request.setRemoteCode("esuO1V6j");
-        System.out.println(toJson(request));
+
     }
-
 
 }
