589837784f209fd9c864b8277f359780c786b22f
rui.zhang <rui.zhang@tendcloud.com>
2020-02-25 08:39:49 +0800

    调整
    Change-Id: Ie680d6c0499c858db963491434967ee7757f1243

---
+++ src/test/java/org/chobit/calf/service/UserServiceTest.java
@@ -0,0 +1,23 @@
+package org.chobit.calf.service;
+
+import org.chobit.calf.TestBase;
+import org.chobit.calf.utils.MD5;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
+
+/**
+ * @author robin
+ */
+public class UserServiceTest extends TestBase {
+
+
+    @Value("${calf.passoword-salt}")
+    private String passwordSalt;
+
+    @Test
+    public void encode(){
+        System.out.println(MD5.encode(passwordSalt + "admin"));
+    }
+
+
+}
--- src/test/java/org/chobit/calf/spring/CalfConfigTest.java
+++
@@ -1,7 +0,0 @@
-package org.chobit.calf.spring;
-
-import org.chobit.calf.TestBase;
-
-public class CalfConfigTest extends TestBase {
-
-}
