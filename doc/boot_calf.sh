06712c61315c15559012bf39649aa5f5a453a871
rui.zhang <rui.zhang@tendcloud.com>
2020-03-14 21:16:40 +0800

    调整启动脚本
    Change-Id: I579d8b2b7734237bff792a97cd2260562555d37a

--- doc/boot_calf.sh
+++ doc/boot_calf.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/nohup /usr/bin/java -server -Xms128m -Xmx256m -jar /www/mihuantang/calf.jar >> /www/mihuantang/mihuantang.out &
+/usr/bin/nohup /usr/bin/java -server -Xms128m -Xmx256m -jar /www/xxx/calf.jar >> /www/xxx/xxx.out &
